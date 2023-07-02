d37d0954c5f1623b2083ab3c6ce609214bda45c0
appleappleapplenanner <chetfalcon@gmail.com>
2023-07-02 07:56:30 -0700

    Add support for passing video bytes

--- vpdqpy/src/vpdqpy/__main__.py
+++ vpdqpy/src/vpdqpy/__main__.py
@@ -1,6 +1,8 @@
 import logging
 from typing import Optional, Annotated, List
 from pathlib import Path
+import subprocess
+import json
 
 from pdqhashing.hasher.pdq_hasher import PDQHasher
 
@@ -31,99 +33,50 @@ class PDQFrame:
     quality: int
     timeStamp: float # This is frameNumber / framesPerSec
 
+def get_vid_info(file: bytes) -> dict:
+    # ffprobe command to get info. ffmpeg-python requires a file name, this does not.
+    ffprobe_process = subprocess.Popen(
+        ['ffprobe', '-show_streams', '-print_format', 'json', '-'],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
 
-class PDQFrameBufferHasher:
-    """
-    //  Variables for computing pdq hash
-    std::vector<float> _fullLumaImageBuffer1;
-    std::vector<float> _fullLumaImageBuffer2;
-    static const int SCALED_DIMENSION = 64;
-    float _buffer64x64[64][64];
-    float _buffer16x64[16][64];
-    float _buffer16x16[16][16];
-    """
-    SCALED_DIMENSION: int = 64
-    FEATURE_DIMENSION: int = 256
-    MIN_HASHABLE_DIM: int = 5
-    
-    _fullLumaImageBuffer1: List[float] = [] # Size is _numRGBTriples
-    _fullLumaImageBuffer2: List[float] = [] # Size is _numRGBTriples
-    _buffer64x64: List[List] = [[]]
-    _buffer16x64: List[List] = [[]]
-    _buffer16x16: List[List] = [[]]
+    # Retrieve the output and error streams from ffprobe
+    stdout, stderr = ffprobe_process.communicate(input=file)
 
-    def __init__(self, frameHeight: int, frameWidth: int):
-        self._frameHeight = frameHeight
-        self._frameWidth = frameWidth
-        self._numRGBTriples = frameHeight * frameWidth
+    # Decode the output stream as json
+    output = json.loads(stdout.decode('utf-8'))
+    error = stderr.decode('utf-8')
 
-    """
-    // Get PDQ Hash in Hash256 format
-    bool PDQFrameBufferHasher::hashFrame(
-        unsigned char* buffer,
-        pdq::hashing::Hash256& hash, // The result pdq hash
-        int& quality // Hashing Quality
-    ) {
-    if (_frameHeight < MIN_HASHABLE_DIM || _frameWidth < MIN_HASHABLE_DIM) {
-        hash.clear();
-        quality = 0;
-        return false;
-    }
-    facebook::pdq::hashing::fillFloatLumaFromRGB(
-        &buffer[0], // pRbase
-        &buffer[1], // pGbase
-        &buffer[2], // pBbase
-        _frameHeight,
-        _frameWidth,
-        3 * _frameWidth, // rowStride
-        3, // colStride
-        _fullLumaImageBuffer1.data());
-
-    facebook::pdq::hashing::pdqHash256FromFloatLuma(
-        _fullLumaImageBuffer1.data(), // numRows x numCols, row-major
-        _fullLumaImageBuffer2.data(), // numRows x numCols, row-major
-        _frameHeight,
-        _frameWidth,
-        _buffer64x64,
-        _buffer16x64,
-        _buffer16x16,
-        hash,
-        quality);
-
-    return true;
-    }
-
-    };
-    """
-
-def get_vid_info(video: Path | str):
-    probe = ffmpeg.probe(video)
-    video_info = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+    video_info = next((stream for stream in output['streams'] if stream['codec_type'] == 'video'), None)
     if not video_info:
         raise ValueError("No video stream found in the input file.")
     return video_info
 
-def main():
-    pdq = PDQHasher()
+# Get the bytes of a video
+def get_video_bytes(video_file: Path | str | bytes) -> bytes:
+    video: bytes = None
+    if isinstance(video_file, (Path, str)):
+        try:
+            with open(str(video_file), 'rb') as file:
+                video = file.read()
+        except OSError:
+            raise ValueError("Failed to get video file bytes. Invalid object type.")
+    elif isinstance(video_file, bytes):
+        video = video_file
+    else:
+        raise ValueError("Failed to get video file bytes. Invalid object type.")
+
+    return video
+
+# Perceptually hash video from a file path or the bytes
+def phash_video(video_file: Path | str | bytes) -> list[PDQFrame]:
+    video = get_video_bytes(video_file)
+    if video is None:
+        return
     
-    testdir = Path(__file__).parent.parent.parent / "tests/vids"
-    testpic = testdir / "grass.jpeg"
-    testvid1 = testdir / "Big_Buck_Bunny_720_10s_1MB.mp4"
-    testpic_pdq = pdq.fromFile(filepath=str(testpic))
-    print(testpic_pdq.getHash())
-
-
-    interval = 1/1 # How often to get a frame in seconds / frame
-
-    out, _= (
-            ffmpeg
-            .input(testvid1)
-            .output('pipe:', format='rawvideo', pix_fmt='rgb24', r=interval)
-            .run(capture_stdout=True)
-        )
-    
-
-    video_info = get_vid_info(testvid1)
+    video_info = get_vid_info(video)
     print(video_info)
     width = int(video_info['width'])
     height = int(video_info['height'])        
@@ -131,21 +84,40 @@ def main():
     duration = float(video_info['duration'])
     print(f"{framerate} FPS, {duration} seconds")
 
-    video = np.frombuffer(out, np.uint8).reshape([-1, height, width, 3])
+    interval = 1/1 # How often to get a frame in seconds / frame
 
-    pdqHashes = []
-    for frameNum, frame in enumerate(tqdm(video)):
+    out, _= (
+            ffmpeg
+            .input('pipe:')
+            .output('pipe:', format='rawvideo', pix_fmt='rgb24', r=interval)
+            .run(input=video, capture_stdout=True)
+        )
+    
+
+    video_frames = np.frombuffer(out, np.uint8).reshape([-1, height, width, 3])
+
+    pdq = PDQHasher()
+    pdqHashes: list[PDQFrame] = []
+    for frameNum, frame in enumerate(tqdm(video_frames)):
         image = Image.fromarray(frame)
         pdqHashAndQuality = pdq.fromBufferedImage(image)
         timestamp = 10 * (frameNum) / (duration * interval) # in seconds
         pdqFrame = PDQFrame(pdqHashAndQuality.hash, frameNum,
                             pdqHashAndQuality.quality, timestamp)
         pdqHashes.append(pdqFrame)
-    print(pdqHashes)
 
-        
+    return pdqHashes
 
-    #image.save("frame.jpg", format="JPEG")
+def main():
+    
+    testdir = Path(__file__).parent.parent.parent / "tests/vids"
+    testvid1 = testdir / "Big_Buck_Bunny_720_10s_1MB.mp4"
+    # Read the file into bytes
+    with open(testvid1, 'rb') as file:
+        video_bytes = file.read()
+
+    print(phash_video(video_bytes))
+
     typer.Exit()
 
 try:
